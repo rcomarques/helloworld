be00e70efc29967acca6eebb79b1cf9ab85747f2
Rafael Marques <gemeo.rafa98@gmail.com>
2019-03-26 13:08:43 +0000

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,5 @@
-public class HelloWorld {
-
+public class Main {
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, World!");
     }
-
 }
